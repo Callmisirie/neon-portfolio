3942a0ae89d7ed784b7bd3dfe1ef996b2126c3fd
Callmisirie <kensirie@gmail.com>
2024-03-23 10:52:42 +0100

    config tailwindcss to your react app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,53 +1,62 @@
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import Home from "./pages/home.jsx"
-import Manga from "./pages/manga.jsx";
-import Chapters from "./pages/chapters.jsx";
-import Pages from "./pages/pages.jsx";
-import Contact from "./pages/contact.jsx";
-import About from "./pages/about.jsx";
-import Find from "./pages/find.jsx";
+// // import './index.css'
+// import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
+// import Home from "./pages/home.jsx"
+// import Manga from "./pages/manga.jsx";
+// import Chapters from "./pages/chapters.jsx";
+// import Pages from "./pages/pages.jsx";
+// import Contact from "./pages/contact.jsx";
+// import About from "./pages/about.jsx";
+// import Find from "./pages/find.jsx";
 
-import Manager from "./pages/manager.jsx";
-import { Create, Delete, Edit } from "./pages/manager.jsx";
-import Auth from "./pages/auth.jsx";
+// import Manager from "./pages/manager.jsx";
+// import { Create, Delete, Edit } from "./pages/manager.jsx";
+// import Auth from "./pages/auth.jsx";
 
-import Navbar from "./components/Navbar.jsx";
+// import Navbar from "./components/Navbar.jsx";
 
-import { useCookies } from "react-cookie";
+// import { useCookies } from "react-cookie";
 
 
-function App() {
-  const [cookies, ] = useCookies(["access_token"]);
+// function App() {
+//   const [cookies, ] = useCookies(["access_token"]);
 
+//   return (
+//     <div className="App">
+//       <Router>
+//         <Navbar/>
+//         <Routes>  
+//           <Route path="/" element={<Home />}/>
+//           <Route path="/contact" element={<Contact />}/>
+//           <Route path="/about" element={<About />}/>
+//           <Route path="/manga" element={<Manga />}/>
+//           <Route path="/auth" element={<Auth />}/>
+//           <Route path="/manga/find/:mangaName" element={<Find />}/>
+//           {cookies.access_token && (
+//             <>
+//               <Route path="/manager" element={<Manager />}/>
+//               <Route path="/manager/create/manga" element={<Create />}/>
+//               <Route path="/manager/create/manga/chapter" element={<Create />}/>
+//               <Route path="/manager/delete/manga" element={<Delete />}/>
+//               <Route path="/manager/delete/manga/chapter" element={<Delete />}/>
+//               <Route path="/manager/edit/manga" element={<Edit />}/>
+//               <Route path="/manager/edit/manga/chapter" element={<Edit />}/>
+//             </>
+//           )}
+//           <Route path="/manga/:chapters" element={<Chapters />}/>
+//           <Route path="/manga/:chapters/:pages" element={<Pages />}/>
+//         </Routes>
+//       </Router>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+export default function App() {
   return (
-    <div className="App">
-      <Router>
-        <Navbar/>
-        <Routes>  
-          <Route path="/" element={<Home />}/>
-          <Route path="/contact" element={<Contact />}/>
-          <Route path="/about" element={<About />}/>
-          <Route path="/manga" element={<Manga />}/>
-          <Route path="/auth" element={<Auth />}/>
-          <Route path="/manga/find/:mangaName" element={<Find />}/>
-          {cookies.access_token && (
-            <>
-              <Route path="/manager" element={<Manager />}/>
-              <Route path="/manager/create/manga" element={<Create />}/>
-              <Route path="/manager/create/manga/chapter" element={<Create />}/>
-              <Route path="/manager/delete/manga" element={<Delete />}/>
-              <Route path="/manager/delete/manga/chapter" element={<Delete />}/>
-              <Route path="/manager/edit/manga" element={<Edit />}/>
-              <Route path="/manager/edit/manga/chapter" element={<Edit />}/>
-            </>
-          )}
-          <Route path="/manga/:chapters" element={<Chapters />}/>
-          <Route path="/manga/:chapters/:pages" element={<Pages />}/>
-        </Routes>
-      </Router>
-    </div>
-  );
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
 }
-
-export default App;
